394d98f5e5d21dbca6d462c5eaa135f7592c65fe
CHETHAN REDDY <chethan.p@techiemaya.com>
2025-07-23 10:34:18 +0530

    main.py

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ app.mount("/static", StaticFiles(directory=os.path.dirname(__file__)), name="sta
 
 if __name__ == "__main__":
     import uvicorn
+    import os
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
  
